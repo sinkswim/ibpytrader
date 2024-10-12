916671475446fde7dbe312cfb6359186076ba2be
sinkswim <margellirobert@gmail.com>
2024-10-12 12:26:13 +0200

    Initial app files setup

---
+++ client.py
@@ -0,0 +1,5 @@
+from ibapi.client import EClient
+
+class IBClient(EClient):
+    def __init__(self, wrapper):
+        EClient.__init__(self, wrapper)
---
+++ wrapper.py
@@ -0,0 +1,4 @@
+from ibapi.wrapper import EWrapper
+class IBWrapper(EWrapper):
+    def __init__(self):
+        EWrapper.__init__(self)
